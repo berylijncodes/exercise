a9220dbb0671a03d1d4723c2b975577ba2daee2e
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-06 16:46:49 +0100

    Use Geolocator_Api_key from .env

--- runner.py
+++ runner.py
@@ -1,14 +1,19 @@
 import os
+from dotenv import load_dotenv
 
 from geolocator.client import Client
 from geolocator.database import Database
 from geolocator.user_interface import UserInterface
 
+load_dotenv()
+
 db_path = os.path.join(os.path.dirname(__file__), "db", "database.db")
 database = Database()
 database.connect(db_path)
 
-client = Client("YOUR_API_KEY")
+geolocator_api_key = os.environ.get("GEOLOCATOR_API_KEY")
+
+client = Client(geolocator_api_key)
 ui = UserInterface()
 
 # wire everything up below:
