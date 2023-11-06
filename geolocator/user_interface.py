afb51965b816741a758df49dcb56885ae514e20c
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-06 19:20:12 +0100

    Add function to prompt user for latitude & logitude

--- geolocator/user_interface.py
+++ geolocator/user_interface.py
@@ -2,3 +2,9 @@ class UserInterface:
     def hello(self):
         answer = input("Question?: ")
         print(answer)
+
+    def get_location(self):
+        latitude = input("Fill in your Latitude: ")
+        longitude = input("Fill in your Longitude: ")
+
+        return latitude, longitude
