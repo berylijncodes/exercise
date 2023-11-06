9178871f755a119ff7e994ddffb07b3ee0daa2be
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-06 19:03:49 +0100

    Update client GET method

--- geolocator/client.py
+++ geolocator/client.py
@@ -2,8 +2,21 @@ import requests
 
 
 class Client:
+limit = 10
+
     def __init__(self, api_key):
         self._api_key = api_key
 
-    def get(self, latitude, longitude):
-        pass
+    def get(self, latitude, longitude,):
+        url = f"http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&limit={self.limit}&appid={self._api_key}"
+
+        print("Making API request.", url)
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception("Error: Api request unsuccessful.")
+
+        raw_data = response.json()
+
+        return raw_data
