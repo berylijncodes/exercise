1cd52fdd96dfdb501076d7530a83d38525eb6b03
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-09 11:51:35 +0100

    Fix user interface test

--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -9,13 +9,17 @@ class TestUserInterface(TestCase):
         self.ui = UserInterface()
 
     def test_get_location(self):
-        input_data = "51.5128\n-0.0918\n"
+        input_data = "51.5128,-0.0918\n"
         expected_latitude = 51.5128
         expected_longitude = -0.0918
+
+        original_stdin = sys.stdin
 
         sys.stdin = StringIO(input_data)
 
         latitude, longitude = self.ui.get_location()
+
+        sys.stdin = original_stdin
 
         self.assertEqual(latitude, expected_latitude)
         self.assertEqual(longitude, expected_longitude)
