b49134943825e86651fdad2b7c4fb5797d303f69
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-09 11:51:47 +0100

    Test database

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,5 +9,27 @@ class TestDatabase(TestCase):
         self.database.connect(':memory:')
 
     def test_can_write_to_geolocation_table(self):
-        self.database.write(51.5128, -0.0918, "London", "GB")
-        self.assertEqual(2 + 2 , 4)
+        longitude = 123
+        latitude = 456
+        city = "London"
+        country = "GB"
+
+        self.database.write(latitude, longitude, city, country)
+
+        cursor = self.database.connection.cursor()
+        cursor.execute(
+            """
+            SELECT latitude, longitude, city, country_code
+            FROM geolocation
+            WHERE latitude = ? AND longitude = ?;
+            """,
+            (latitude, longitude)
+        )
+
+        row = cursor.fetchone()
+
+        self.assertEqual(row[0], latitude)
+        self.assertEqual(row[1], longitude)
+        self.assertEqual(row[2], city)
+        self.assertEqual(row[3], country)
+        
