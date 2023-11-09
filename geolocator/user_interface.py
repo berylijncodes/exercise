ad4fa36b34af8db83f393624e72573dacc3d16ec
Beryl Ilenwabor <berylijn@gmail.com>
2023-11-09 11:32:46 +0100

    Create test suite for user interface

--- geolocator/user_interface.py
+++ geolocator/user_interface.py
@@ -1,8 +1,4 @@
 class UserInterface:
-    def hello(self):
-        answer = input("Question?: ")
-        print(answer)
-
     def get_location(self):
         location_input = input("Fill in your Latitude and Longitude (comma-separated): ")
         latitude, longitude = map(float, location_input.split(','))
